1ce79cb08020758759e4529523a721ac8366f70d
Juan Grajales <grajales.juanfe11@gmail.com>
2019-12-19 15:02:17 -0500

    create add task button, have to pull first to get new updates for Task.jsx

--- src/Containers/TaskPage/TaskList.jsx
+++ src/Containers/TaskPage/TaskList.jsx
@@ -1,19 +1,36 @@
 import React, { Component } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 
 // Custom Components
 import Welcome from "../../Components/Messages/Welcome";
 import ShowTasks from "../../Components/Task/ShowTasks";
+import AddTask from "../../Components/test-tasks/addTask"
 
 export default class TaskList extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showAddButton: false
+    };
+  }
+
+  toogleTaskMenu = () => {
+    this.setState({ 
+      showAddButton: !this.state.showAddButton 
+    });
+  };
 
   render() {
     return (
       <Container>
         <Welcome {...this.props} />
+        <Button onClick={this.toogleTaskMenu}>Add Task</Button>
+        {this.state.showAddButton ? (
+          <AddTask fetchData={this.props.fetchData} showAddTaskMenu={this.addTaskMenu}/>
+        ) : (
+          ""
+        )}
+
         <ShowTasks tasks={this.props.viewableTasks} />
       </Container>
     );
