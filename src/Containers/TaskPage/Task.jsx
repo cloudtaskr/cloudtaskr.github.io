8681aa670079ac2be38b4894a756881af08c51fc
avrahm <me@avrahm.com>
2019-12-19 20:03:58 -0500

    fixed tasklist to test-tasklist

--- src/Containers/TaskPage/Task.jsx
+++ src/Containers/TaskPage/Task.jsx
@@ -11,57 +11,30 @@ import {
 
 // Project Components
 import Menu from "../../Components/Menu/Menu";
-// import TaskList from "../../Containers/TaskPage/TaskList";
-import TaskList from "../../Components/test-tasks/taskList"
-// import AddTaskModal from "../../Components/test-tasks/addTaskModal";
+import TaskList from "../../Containers/TaskPage/TaskList";
+import AddTaskModal from "../../Components/test-tasks/addTaskModal";
 
 // Styling
 import "../../Components/Task/Task.css";
-// import { LinkContainer } from "react-router-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default class Task extends Component {
-  state = {
-    allTasks: [],
-    displayedTasks: [],
-    targetName: "task"
-  };
-
-  checkForTasks = e => {
-    let tasks;
-    let count = this.props.filterTaskList.length;
-
-    if (count > 0) {
-      tasks = this.props.filterTaskList;
+  checkForTasks = () => {
+    if (this.props.filterTaskList.length > 0) {
+      return this.props.filterTaskList;
     } else {
-      tasks = [];
+      return {};
     }
-
-    let targetName = e.target.getAttribute("name");
-
-    this.setState({ allTasks: [...tasks], targetName: targetName }, () => {
-      this.updateView();
-    });
   };
 
-  updateView = () => {
-    let view = this.state.targetName;
-    let display;
-
-    switch (view) {
-      case "task":
-        display = this.state.allTasks;
-        break;
-      default:
-        display = [];
-    }
-
-    this.setState({ displayedTasks: [...display] });
-  };
+  // addTasks = () => {
+  //   return <AddTaskModal />;
+  // };
 
   render() {
     return (
       <>
-        <Menu
+        <Menu 
           id="landingMenuSticky"
           {...this.props}
           logout={this.props.logout}
@@ -72,53 +45,50 @@ export default class Task extends Component {
           <aside id="task-sidebar">
             <Container>
               <h2 className="sidebar-header">Sections</h2>
-              <ul className="fa-ul">
-                <li>
-                  <h4
-                    name="task"
-                    className="sidebar-item"
-                    onClick={this.checkForTasks}
-                  >
-                    <FontAwesomeIcon icon={faCloud} listItem />
-                    Tasks
-                  </h4>
-                </li>
-                <li>
-                  <h4
-                    name="inbox"
-                    className="sidebar-item"
-                    onClick={this.checkForTasks}
-                  >
-                    <FontAwesomeIcon icon={faInbox} listItem />
-                    Inbox
-                  </h4>
-                </li>
-              </ul>
+              <LinkContainer className="sidebar-item" to="/task/all">
+                <ul className="fa-ul">
+                  <li>
+                    <h4>
+                      <FontAwesomeIcon icon={faCloud} listItem />
+                      Cloud Tasks
+                    </h4>
+                  </li>
+                </ul>
+              </LinkContainer>
+              <LinkContainer className="sidebar-item" to="/task/inbox">
+                <ul className="fa-ul">
+                  <li>
+                    <h4>
+                      <FontAwesomeIcon icon={faInbox} listItem />
+                      Inbox
+                    </h4>
+                  </li>
+                </ul>
+              </LinkContainer>
 
               <h3 className="sidebar-header">Zones</h3>
-
-              <ul className="fa-ul">
-                <li>
-                  <h4
-                    name="home"
-                    className="sidebar-item"
-                    onClick={this.checkForTasks}
-                  >
-                    <FontAwesomeIcon icon={faHome} listItem />
-                    Home
-                  </h4>
-                </li>
-                <li>
-                  <h4
-                    name="work"
-                    className="sidebar-item"
-                    onClick={this.checkForTasks}
-                  >
-                    <FontAwesomeIcon icon={faBriefcase} listItem />
-                    Work
-                  </h4>
-                </li>
-              </ul>
+              <div className="sidebar-item">
+                <LinkContainer to="/task/home">
+                  <ul className="fa-ul">
+                    <li>
+                      <h4>
+                        <FontAwesomeIcon icon={faHome} listItem />
+                        Home
+                      </h4>
+                    </li>
+                  </ul>
+                </LinkContainer>
+              </div>
+              <LinkContainer className="sidebar-item" to="/task/work">
+                <ul className="fa-ul">
+                  <li>
+                    <h4>
+                      <FontAwesomeIcon icon={faBriefcase} listItem />
+                      Work
+                    </h4>
+                  </li>
+                </ul>
+              </LinkContainer>
 
               <h3 className="sidebar-header">Tags</h3>
               <Button id="add-task">
@@ -128,17 +98,9 @@ export default class Task extends Component {
               </Button>
             </Container>
           </aside>
-
           <section id="task-section">
-            <TaskList
-              {...this.props}
-              viewableTasks={this.state.displayedTasks}
-              // viewableTasks={this.props.filterTaskList}
-              fetchData={this.props.fetchData}
-
-            />
+            <TaskList {...this.props} viewableTasks={this.checkForTasks()} />
           </section>
-          
         </div>
       </>
     );
